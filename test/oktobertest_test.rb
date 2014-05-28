25a1aa7c08c9516b9f28ff3b2dce2da16b2d77ad
Patricio Mac Adden <patriciomacadden@gmail.com>
2014-05-27 23:02:45 -0300

    fix tests

--- test/oktobertest_test.rb
+++ test/oktobertest_test.rb
@@ -3,7 +3,7 @@ require 'helper'
 scope do
   test 'successful run' do
     expected = ".\n"
-    output = %x(ruby test/fixtures/successful_test.rb)
+    output = %x(ruby -I lib:test test/fixtures/successful_test.rb)
     assert expected == output
   end
 
@@ -15,7 +15,7 @@ error: RuntimeError
 file: test/fixtures/error_test.rb
 line: 4
 EOS
-    output = %x(ruby test/fixtures/error_test.rb)
+    output = %x(ruby -I lib:test test/fixtures/error_test.rb)
     assert expected == output
   end
 
@@ -27,7 +27,7 @@ error: condition is not true: false
 file: test/fixtures/failing_test.rb
 line: 4
 EOS
-    output = %x(ruby test/fixtures/failing_test.rb)
+    output = %x(ruby -I lib:test test/fixtures/failing_test.rb)
     assert expected == output
   end
 
@@ -39,19 +39,19 @@ skip
 file: test/fixtures/skipped_test.rb
 line: 4
 EOS
-    output = %x(ruby test/fixtures/skipped_test.rb)
+    output = %x(ruby -I lib:test test/fixtures/skipped_test.rb)
     assert expected == output
   end
 
   test 'run only one scope' do
     expected = "..\n"
-    output = %x(SCOPE='run this' ruby test/fixtures/run_scope_test.rb)
+    output = %x(SCOPE='run this' ruby -I lib:test test/fixtures/run_scope_test.rb)
     assert expected == output
   end
 
   test 'run only one test' do
     expected = ".\n"
-    output = %x(TEST='run this' ruby test/fixtures/run_test_test.rb)
+    output = %x(TEST='run this' ruby -I lib:test test/fixtures/run_test_test.rb)
     assert expected == output
   end
 end
