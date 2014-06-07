0ae798942148730a09d3230792fc1eee2816a8ee
Patricio Mac Adden <patriciomacadden@gmail.com>
2014-06-07 18:02:53 -0300

    fix tests

--- test/oktobertest_test.rb
+++ test/oktobertest_test.rb
@@ -45,13 +45,13 @@ EOS
 
   test 'run only one scope' do
     expected = "..\n"
-    output = %x(SCOPE='run this' ruby -I lib:test test/fixtures/run_scope_test.rb)
+    output = %x(S='run this' ruby -I lib:test test/fixtures/run_scope_test.rb)
     assert expected == output
   end
 
   test 'run only one test' do
     expected = ".\n"
-    output = %x(TEST='run this' ruby -I lib:test test/fixtures/run_test_test.rb)
+    output = %x(T='run this' ruby -I lib:test test/fixtures/run_test_test.rb)
     assert expected == output
   end
 end
